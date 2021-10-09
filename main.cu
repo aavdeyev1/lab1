7837b15fa2f46714ee77b0aac08e92d7c43470f9
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2021-10-09 14:21:42 -0700

    f

--- main.cu
+++ main.cu
@@ -74,8 +74,7 @@ int main( int argc, char* argv[] )
     blockSize = (int) atoi(argv[2]);
  
     // Number of thread blocks in grid
-    gridSize = 1024;
-   //  gridSize = (int)ceil((double)((double)((N+1)/2)/blockSize));
+    gridSize = (int)ceil((double)((double)((N)/2)/blockSize));
  
     // Execute the kernel
     vec1<<<gridSize, blockSize>>>(d_input, d_output, N);
