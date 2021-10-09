b7bc6a1587b5a6a28e33e833255d8e3e3890991b
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2021-10-08 22:08:19 -0700

    8

--- main.cu
+++ main.cu
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <sys/time.h>
+#include <time.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
  
 typedef unsigned long long bignum;
 
