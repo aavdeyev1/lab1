7ad57e34f3a602d26e02b6b98abdacc5aa51f7ad
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2021-10-08 23:47:57 -0700

    9

--- main.cu
+++ main.cu
@@ -22,13 +22,13 @@ __global__ void vec1(double *a, double *result, int n)
 void printArray(char a[], int len){
  
     int i;
-    printf("\n[")
+    printf("\n[");
     for(i=0; i<len; i++){
     
        printf("%d, ", a[i]);
  
     }
-    printf("]\n")
+    printf("]\n");
  
  }
  
