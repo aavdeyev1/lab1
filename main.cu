710873de1fabd79ea80d032583752c010313459a
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2021-10-09 00:02:22 -0700

    543

--- main.cu
+++ main.cu
@@ -8,7 +8,7 @@
 typedef unsigned long long bignum;
 
 // CUDA kernel. Each thread takes care of one element of c
-__global__ void vec1(double *a, double *result, int n)
+__global__ void vec1(bignum *a, bignum *result, int n)
 {
     // Get our global thread ID
     int id = blockIdx.x*blockDim.x+threadIdx.x;
@@ -19,13 +19,13 @@ __global__ void vec1(double *a, double *result, int n)
 }
 
 
-void printArray(char a[], int len){
+void printArray(bignum * a, int len){
  
     int i;
     printf("\n[`");
     for(i=0; i<len; i++){
     
-       printf("%d, ", a[i]);
+       printf("%llu, ", a[i]);
  
     }
     printf("]\n");
@@ -43,6 +43,28 @@ int main( int argc, char* argv[] )
     // Retrieve N, blockSize from args
     bignum N = (bignum) atoi(argv[1]);
     int blockSize = (int) atoi(argv[2]);
+
+    size_t bytes = N * sizeof(bignum);
+
+    bignum *h_input;
+    bignum *h_output;
+
+    h_input = (bignum *)malloc(bytes);
+    h_output = (bignum *)malloc(bytes);
+
+    int i;
+    for (i=0; i < N; i++){
+      h_input[i] = i;
+      h_output[i] = 0;    
+    }
+    printArray(h_input, N);
+    printArray(h_output, N);
+
+    
+    free(h_input);
+    free(h_output);
+
+
 
     return 0;
 }
